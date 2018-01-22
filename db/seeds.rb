f36627b38ad1a528bac06c42b963dfebd2244abb
Rupert Saxton <rsaxton@covermymeds.com>
2018-01-22 13:39:47 -0500

    Add a seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lists = List.create([{ name: "Onboarding" }, { name: "Other Stuff" }])
+Task.create(description: "Read Documentation", completed: false, list: lists.first)
+Task.create(description: "Do Tasks App", completed: true, list: lists.first)
+Task.create(description: "Get Code Review", completed: false, list: lists.first)
+
+Task.create(description: "Learn how to use confluence", completed: false, list: lists.last)
+Task.create(description: "Get a snack", completed: false, list: lists.last)
+
